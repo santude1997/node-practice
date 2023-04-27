ca582be7090f26ad88a1f781092abd87e17def63
Santu De <Santu.De@inindcrws.com>
2023-04-27 22:48:53 +0530

    understanding import and export

---
+++ importExports/operator.js
@@ -0,0 +1,18 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const mul = (a, b) => {
+  return a * b;
+};
+// module.exports.add = add;
+// module.exports.sub=sub;
+// module.exports.mul=mul;
+
+const name = "santu";
+
+module.exports = { add, sub, mul, name };
