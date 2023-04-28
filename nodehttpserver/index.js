23e9e6eb150909f41e2204ed3322813dfa68ceef
Santu De <Santu.De@inindcrws.com>
2023-04-28 12:25:10 +0530

    server created with status code

--- nodehttpserver/index.js
+++ nodehttpserver/index.js
@@ -1,23 +1,27 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-if(req.url=='/'){
-    res.writeHead(200,{"content-type":"text/html"})
-    res.write("Hello from Home page")
-    res.end()
-}else if(req.url=='/about'){
-    res.writeHead(200,{"content-type":"text/html"})
-    res.write("Hello from About page")
-    res.end()
-}else if(req.url=='/contact'){
-    res.writeHead(200,{"content-type":"text/html"})
-    res.write("Hello from Contact page")
-    res.end()
-}else{
-    res.writeHead(404,{"content-type":"text/html"})
-    res.write("404 error")
-    res.end()
-}
+  if (req.url == "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("Hello from Home page");
+    res.end();
+
+  } else if (req.url == "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("Hello from About page");
+    res.end();
+
+  } else if (req.url == "/contact") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("Hello from Contact page");
+    res.end();
+
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("404 error");
+    res.end();
+  }
+  
 });
 
 server.listen(7000, "127.0.0.1", () => {
